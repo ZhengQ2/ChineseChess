6b5c24eb807a6b0b8b53ed340c2b6aceb074d4da
ZhengQ2 <77452278+ZhengQ2@users.noreply.github.com>
2021-01-15 03:38:21 +0800

    Update ChineseChess.java

--- ChineseChess.java
+++ ChineseChess.java
@@ -1,21 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chinesechess;
 
-/**
- *
- * @author Zheng Qiu
- */
 public class ChineseChess {
 
-    /**
-     * @param args the command line arguments
-     */
+    int[][] chessBoard = new int[9][10];
+    Chess[] redChess = new Chess[16];
+    Chess[] blackChess = new Chess[16];
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public void pushButtons(int chessRule, boolean red, int chessNum, int xPosMove, int yPosMove) {
+        boolean move;
+        int oriXPos, oriYPos;
+
+        if (red) {
+            oriXPos = redChess[chessNum].getXPos();
+            oriYPos = redChess[chessNum].getYPos();
+
+            move = redChess[chessNum].move(xPosMove, yPosMove);
+        } else {
+            oriXPos = blackChess[chessNum].getXPos();
+            oriYPos = blackChess[chessNum].getYPos();
+
+            move = blackChess[chessNum].move(xPosMove, yPosMove);
+        }
+        
+        int newXPos, newYPos;
+
+        if (move) {
+            if (red) {
+                newXPos = redChess[chessNum].getXPos();
+                newYPos = redChess[chessNum].getYPos();
+            } else {
+                newXPos = blackChess[chessNum].getXPos();
+                newYPos = blackChess[chessNum].getYPos();
+            }
+            
+            if (chessBoard[newXPos+1][newYPos+1] != 0) {
+                recordEat();
+            }
+            
+            checkWinner();
+
+        }
+
+    }
+
 }
