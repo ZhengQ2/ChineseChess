7159d99aafff51c2704a5877f90544f9f3aa8e43
ZhengQ2 <77452278+ZhengQ2@users.noreply.github.com>
2021-01-18 23:40:19 +0800

    Update Guard.java

--- Guard.java
+++ Guard.java
@@ -11,16 +11,17 @@ public class Guard extends Chess {
         super(xPos, yPos, red);
     }
 
-    public boolean move(int yMove, int xMove, boolean canMove) {
-
+    public boolean move(int yMove, int xMove) {
+        boolean canMove = false;
+        
         if (red) {
-            if (((Math.abs(yMove) == 1 && Math.abs(xMove) == 1)) && canMove && (yPos + yMove) >= 1 && (yPos + yMove) <= 3 && (xPos + xMove) >= 4 && (xPos + xMove) <= 6) {
+            if (((Math.abs(yMove) == 1 && Math.abs(xMove) == 1)) && (yPos + yMove) >= 1 && (yPos + yMove) <= 3 && (xPos + xMove) >= 4 && (xPos + xMove) <= 6) {
                 xPos += xMove;
                 yPos += yMove;
                 canMove = true;
             }
         } else {
-            if (((Math.abs(yMove) == 1 && Math.abs(xMove) == 1)) && canMove && (yPos + yMove) >= 8 && (yPos + yMove) <= 10 && (xPos + xMove) >= 4 && (xPos + xMove) <= 6) {
+            if (((Math.abs(yMove) == 1 && Math.abs(xMove) == 1)) && (yPos + yMove) >= 8 && (yPos + yMove) <= 10 && (xPos + xMove) >= 4 && (xPos + xMove) <= 6) {
                 xPos += xMove;
                 yPos += yMove;
                 canMove = true;
