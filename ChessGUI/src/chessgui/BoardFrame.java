c0030e5e100f80a5c880225bd44e9e6bf45fc65a
Luyanqiqu <77454687+Luyanqiqu@users.noreply.github.com>
2021-01-18 20:00:23 -0500

    Update BoardFrame.java

--- ChessGUI/src/chessgui/BoardFrame.java
+++ ChessGUI/src/chessgui/BoardFrame.java
@@ -5,34 +5,36 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class BoardFrame extends JFrame implements ActionListener {
+
     Component component;
-    private JButton read;
+
     private JLabel time;
     private int x = 60;
     private final int DELAY = 1000;
     private Timer timer;
-    public BoardFrame()
-    {
+
+    
+
+    public BoardFrame() {
         time = new JLabel("Time: \n" + x + " Seconds");
-        time.setBounds(50,50,70,30);
-
-        read = new JButton("Read/Load");
-        read.addActionListener(this);
-
+        
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
-        panel.setLayout(new GridLayout(0,1));
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
         this.add(panel, BorderLayout.EAST);
         panel.add(time);
-        panel.add(read);
+        panel.add(save);
+        panel.add(load);
 
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setTitle("Chinese Chess");
@@ -45,13 +47,23 @@ public class BoardFrame extends JFrame implements ActionListener {
         this.setVisible(true);
         this.setLayout(null);
     }
-
-    @Override 
-    public void actionPerformed(ActionEvent e){
-
-    }
     
-    public void timeSet(String text) {
-        time.setText(text);
+    JButton save = new JButton(new AbstractAction("Save") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // save Action
+            
+        }
+    });
+
+    JButton load = new JButton(new AbstractAction("Load") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // load Action
+        }
+    });
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
+
 }
